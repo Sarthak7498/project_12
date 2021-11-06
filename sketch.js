1eb0a297b465afc87c2206e43e7fcd68ea67f4cf
Sarthak7498 <90967948+Sarthak7498@users.noreply.github.com>
2021-11-06 16:21:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,65 @@
+var path,boy,cash,diamonds,jewelry,sword;
+var pathImg,boyImg,cashImg,diamondsImg,jewelryImg,swordImg;
+var treasureCollection = 0;
+var cashG,diamondsG,jewelryG,swordGroup;
+
+//Game States
+var PLAY=1;
+var END=0;
+var gameState=1;
+
+function preload(){
+  pathImg = loadImage("Road.png");
+  boyImg = loadAnimation("jake1.png","jake2.png","jake3.png","jake4.png","jake5.png");
+  cashImg = loadImage("cash.png");
+  diamondsImg = loadImage("diamonds.png");
+  jewelryImg = loadImage("jwell.png");
+  swordImg = loadImage("sword.png");
+  
+  
+}
+
+function setup(){
+  
+  createCanvas(400,600);
+// Moving background
+path=createSprite(200,200);
+path.addImage(pathImg);
+path.velocityY = 4;
+
+
+//creating boy running
+boy = createSprite(20,300,50,50);
+boy.addAnimation("SahilRunning",boyImg);
+boy.scale=1;
+  
+  
+cashG=new Group();
+diamondsG=new Group();
+jewelryG=new Group();
+swordGroup=new Group();
+
+}
+
+function draw() {
+
+  if(gameState===PLAY){
+  background(0);
+  boy.x = World.mouseX;
+  
+  edges= createEdgeSprites();
+  boy.collide(edges);
+  
+  //code to reset the background
+  if(path.y > 400 ){
+    path.y = height/2;
+  }
+  
+   
+   
+  
+  drawSprites();
+  
+  }
+
+}
